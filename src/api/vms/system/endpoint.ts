c1b98de00e99f0280080fb2d9d7acd76d452a5fa
dananakbar <dananakbar123321@gmail.com>
2023-03-09 11:25:33 +0700

    Adding path in env

--- src/api/vms/system/endpoint.ts
+++ src/api/vms/system/endpoint.ts
@@ -1,11 +1,11 @@
 // Base URL based on environment variable
 export const baseUrl = `${process.env.NEXT_PUBLIC_API_URL!}/newvms`;
-// export const baseUrlMock = `${process.env.NEXT_PUBLIC_API_URL_MOCK!}/api/v1`;
+export const baseUrlMock = `${process.env.NEXT_PUBLIC_API_URL_MOCK!}`;
 // export const localURL = `${process.env.NEXT_PUBLIC_API_URL_LOCAL!}/api/v1`;
 
 const endpoint = {
   // Add more URL endpoints to request here
-  purchaseOrder: `${baseUrl}/po`,
+  purchaseOrder: `${baseUrlMock}/po`,
 };
 
 export default endpoint;
